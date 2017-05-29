82e0fc3d5bde5724784c351f7333bc131364be8d
colec <colechamberlin@gmail.com>
2017-05-28 21:56:09 -0700

    added election data, factored code better

---
+++ 2016Election.py
@@ -0,0 +1,26 @@
+import NaiveBayesClassifier as nbc
+import pandas as pd
+import numpy as np
+
+if __name__ == '__main__':
+    facts = pd.read_csv('2016-us-election/county_facts.csv',sep=',')
+    facts_features = pd.read_csv('2016-us-election/county_facts_dictionary.csv',sep=',')
+    results = pd.read_csv('2016-us-election/primary_results.csv',sep=',')
+    dem = results[results.party=='Democrat']
+    m = dem.groupby('fips')['fraction_votes'].transform(max) \
+            == dem['fraction_votes']
+    dem_results = pd.merge(facts, dem[m][['fips','candidate']], how='inner', on='fips')
+    dem_results = dem_results.sample(frac=1)
+    rep = results[results.party=='Republican']
+    m = rep.groupby('fips')['fraction_votes'].transform(max) \
+            == rep['fraction_votes']
+    rep_results = pd.merge(facts, rep[m][['fips','candidate']] , how='inner', on='fips')
+    rep_results = rep_results.sample(frac=1)
+    d_data = dem_results.ix[1:,3:dem_results.shape[1]-1]
+    d_labels = dem_results.ix[1:,dem_results.shape[1]-1]
+    r_data = rep_results.ix[1:,3:rep_results.shape[1]-1]
+    r_labels = rep_results.ix[1:,rep_results.shape[1]-1]
+    nbc.REPORTING = False
+    nbc.k_fold(np.array(d_data), np.array(d_labels), 4)
+    nbc.k_fold(np.array(r_data), np.array(r_labels),4)
+    
--- NaiveBayesClassifier.py
+++ NaiveBayesClassifier.py
@@ -3,8 +3,6 @@ import scipy.stats as stats
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-import random
-import time
 
 W = None
 mean = None
@@ -13,22 +11,23 @@ size = None
 cats = None
 images = None
 
+MNIST = False
 REPORTING = True
 
 def train(training_set, labels):
     global mean, std, size, cats
     # categories
     cats = {i:[] for i in np.unique(labels)}
-    mean = np.empty([len(cats),training_set.shape[1]])
-    std = np.empty([len(cats),training_set.shape[1]])
-    size = np.empty([len(cats)])
+    mean = {}
+    std = {}
+    size = {}
     # loop through labels to build up model
     for i, l in enumerate(labels):
         cats[l].append(training_set[i,:])
     for i, l in cats.items():
         class_data = np.array(l)
-        mean[i] = np.mean(class_data.real,axis=0)
-        std[i] = np.std(class_data.real,axis=0)
+        mean[i] = np.mean(class_data,axis=0)
+        std[i] = np.std(class_data,axis=0)
         size[i] = class_data.shape[0]
               
 def joint_prob(x,mu,sigma,size):
@@ -42,36 +41,38 @@ def test(testing_set, testing_labels):
     # count number of correct predictions
     correct = 0
     j_prob_v = np.vectorize(joint_prob)
-    for i, s in enumerate(testing_set):
-        s_image = s
+    for i, e in enumerate(testing_set):
         s_label = testing_labels[i]
         max_label = None
         max_prob = -math.inf
+        # find argmax(cats)
         for c in cats:
             c_mu = mean[c]
             c_sig = std[c]
             c_size = size[c]
-            prior = c_size/len(labels)
+            prior = c_size/len(testing_labels)
             # compute joint probability
             with np.errstate(divide='ignore'):
-                j_prob = np.nansum(j_prob_v(s_image, c_mu, c_sig, c_size))
+                j_prob = np.nansum(j_prob_v(e, c_mu, c_sig, c_size))
             prob = math.log(prior) + j_prob
             # update prediction
             if prob > max_prob:
                 max_label = c
                 max_prob = prob
-        if REPORTING:
+        if MNIST:
             plt.title("Label is {label}, Predicted {pred}".format(label=s_label,pred=max_label))
             pixels = np.array(images[i],dtype="uint8").reshape((28,28))
             plt.imshow(pixels,cmap='gray')
             plt.draw()
             plt.pause(0.01)
+        if REPORTING:
             print("Label: ",s_label)
             print("Predicted: ",max_label)
+        # if prediction was correct
         if s_label == max_label: correct += 1
     print("accuracy: ",correct/len(testing_labels))
 
-def k_fold(data, labels, k_folds):
+def k_fold(data, labels, k_folds, dim_red=False):
     fold_len = data.shape[0] // k_folds
     for k in range(k_folds):
         mask = np.ones(len(data),np.bool)
@@ -82,23 +83,25 @@ def k_fold(data, labels, k_folds):
         # testing split
         testing_set = data[np.invert(mask)]
         testing_labels = labels[np.invert(mask)]
+        # reduce dimension
+        if dim_red > 0:
+            training_set = reduceDim(training_set, dim_red)
+            testing_set = W.dot(testing_set.T).T.real
         train(training_set, training_labels)
         test(testing_set, testing_labels)
 
 def reduceDim(data, small):
+    global W
     cov_mat = np.cov(data, rowvar=False)
     eig_val, eig_vec = np.linalg.eig(cov_mat)
     eig_pairs = [(np.abs(eig_val[i]), eig_vec[:,i]) for i in range(len(eig_val))]
     eig_pairs.sort(key=lambda x: x[0], reverse=True)
-
-    global W
     W = np.matrix([eig_pairs[i][1] for i in range(small)])
-    transformed = W.dot(data.T)
-    return transformed.T.real
+    transformed = W.dot(data.T).T
+    return transformed.real
  
 if __name__ == "__main__":
+    MNIST = True
     mndata = MNIST('samples')
     images, labels = mndata.load_training()
-    transformed = reduceDim(np.array(images), 28)
-    k_fold(transformed, np.array(labels), 100)
-    test(transformed[0:100])
+    k_fold(np.array(images), np.array(labels), 100, 28)
